9d0c22143049f7eb64a60ff382f6e6a022758e02
Ibrahim Aksoy <ib.koray@gmail.com>
2018-06-02 19:11:22 +0300

    Added socket client disconnection log

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ res.sendFile(__dirname + '/index.html');
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
